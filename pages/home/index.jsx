64d2da5dd88f08abf11f33ff15ed4a2fb0d0ab62
onurbakkaloglu <onurbakkaloglu@hotmail.com>
2023-03-21 04:02:14 +0300

    customer component added

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import About from '../../components/About'
 import Campaigns from '../../components/Campaigns'
+import Customers from '../../components/customers/Customers'
 import MenuWrapper from '../../components/product/MenuWrapper'
 import Reservation from '../../components/Reservation'
 import Carousel from '../../components/ui/Carousel'
 
 const index = () => {
   return (
-    <div>
+    <React.Fragment>
       <Carousel/>
       <Campaigns />
       <MenuWrapper />
       <About />
       <Reservation />
-    </div>
+      <Customers/>
+    </React.Fragment>
   )
 }
 
